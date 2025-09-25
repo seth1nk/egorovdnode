25e2537b955d186ed57e100643707c752626a16e
seth1nk <kostik.vs10@mail.ru>
2025-09-25 14:59:47 +0800

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const headerContainer = document.createElement('div');
     headerContainer.className = 'header-container';
     headerContainer.innerHTML = `
-        <a class="pets-link" href="/bicycles/index.html">Велосипеды</a>
+        <a class="pets-link" href="/televisiions/index.html">Телевизоры</a>
         <a class="pets-link" href="/">Главная</a>
-        <a class="nutrition-link" href="/repair-services/index.html">Услуги</a>
+        <a class="nutrition-link" href="/sales/index.html">Продажи</a>
     `;
     document.body.insertBefore(headerContainer, document.body.firstChild);
 });
